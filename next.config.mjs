b5e11061435d9f8602f703fc7ee3d6d0d0ed667a
Akansh Sirohi <akanshjaat@gmail.com>
2024-11-20 00:52:29 +0530

    Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-    assetPrefix = `/${repo}/`
-    basePath = `/${repo}`
-  }
+const repo = "portfolio-static";
+assetPrefix = `/${repo}/`;
+basePath = `/${repo}`;
 
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
     assetPrefix: assetPrefix,
     basePath: basePath,
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
