cd7c6f1062ebff1f2d004392eab2f098d62349aa
Akansh Sirohi <akanshjaat@gmail.com>
2024-11-20 13:25:57 +0530

    Subtitle test update

--- src/app/page.js
+++ src/app/page.js
@@ -35,7 +35,7 @@ export default async function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 mx-6">
-					{`Welcome to the den of a software engineer 🦸‍♂️! I make the magic happen behind the scenes—like a wizard 🧙‍♂️, but with code 💻 (and fewer spells ✨, more caffeine ☕).`}
+					{`I make the magic happen behind the scenes—like a wizard 🧙‍♂️, but with code 💻 (fewer spells ✨, more caffeine ☕).`}
 				</h2>
 			</div>
 		</div>
