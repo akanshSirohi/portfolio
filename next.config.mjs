7686f373447dec9a24abe3f6c817e141171e9ddd
Akansh Sirohi <akanshjaat@gmail.com>
2024-11-20 13:12:38 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const repo = "portfolio-static";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
+// const repo = "portfolio-static";
+// const assetPrefix = `/${repo}/`;
+// const basePath = `/${repo}`;
 
 const nextConfig = {
     output: 'export',
-    assetPrefix: assetPrefix,
-    basePath: basePath,
+    // assetPrefix: assetPrefix,
+    // basePath: basePath,
     images: {
         unoptimized: true,
     }
