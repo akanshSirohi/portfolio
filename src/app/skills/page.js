fe9eb5693be436ab4a6c97bb1db0cef808401a30
Akansh Sirohi <akanshjaat@gmail.com>
2024-11-24 01:53:18 +0530

    Skills UI Fix

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -11,7 +11,7 @@ export default async function Skills() {
     const technologies = data.skills.tecnologies;
 
     return (
-        <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 h-dvh">
+        <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 h-full pb-3.5">
             <Navigation />
             <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
                 <div className="max-w-2xl mx-auto lg:mx-0">
